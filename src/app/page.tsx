b4026910c7631c5c87f0f56c9444bf2caba375e6
unknown <tanvirh103@gmail.com>
2025-07-28 11:05:22 +0600

    build error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import CourseInstructor from "@/component/courseDetails";
 import Welcome from "@/component/welcome";
 import { getDetails } from "@/resources/Api";
 
+export const dynamic = 'force-dynamic';
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ lang?: string }> }) {
   const resolvedSearchParams = await searchParams;
@@ -10,7 +11,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ l
   const data = await getDetails(lang).catch((error) => {
     return console.log(error);
   });
-  if (typeof data === "string") return console.log(data);
+
   return (
     <>
       <Banner />
